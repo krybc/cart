3e7e1216def777d3eb52dfd131b44de3265175fa
Kamil Rybczynski <github@rybczynski.io>
2019-02-05 23:31:31 +0100

    Fix AbstractMigration class namespace, upgrade bin/console file content to new env files structure

--- src/Migrations/Version20180208231907.php
+++ src/Migrations/Version20180208231907.php
@@ -2,15 +2,15 @@
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
 class Version20180208231907 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -28,7 +28,7 @@ class Version20180208231907 extends AbstractMigration
         $this->addSql('ALTER TABLE cart_order_item ADD CONSTRAINT FK_32806FFB4584665A FOREIGN KEY (product_id) REFERENCES cart_product (id)');
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
